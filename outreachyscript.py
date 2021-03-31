6826bb5001d2a608c963626b5de3ff72847b45d4
Ammarpad <ammarpad@yahoo.com>
2021-03-31 19:21:17 +0100

    add script run output

--- outreachyscript.py
+++ outreachyscript.py
@@ -60,19 +60,73 @@ def add_claim_to_item(site, item_id, prop, value):
 
 
 
+"""RUNNING SOME FUNCTIONS OF THE SCRIPT"""
+
 enwiki = pywikibot.Site('en', 'wikipedia')
 wikidata = enwiki.data_repository()
+
+wikidata.login() # Credentials in user-config.py
+
 # Print the content of the page 
 print_outreachy_page(wikidata, 'User:Ammarpad/Outreachy 1')
+
+# MacBook:userscripts Ammar$ python
+# Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27)
+# [Clang 6.0 (clang-600.0.57)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import outreachyscript
+# >>> import pywikibot
+# >>> enwiki = pywikibot.Site('en', 'wikipedia')
+# >>> wikidata = enwiki.data_repository()
+# >>> outreachyscript.print_outreachy_page(wikidata, 'User:Ammarpad/Outreachy 1')
+# (Long text string printed here)
 
 # Append Hello 
 # https://www.wikidata.org/w/index.php?title=User:Ammarpad/Outreachy_1&diff=prev&oldid=1393622205&diffmode=source
 append_hello(wikidata, 'User:Ammarpad/Outreachy 1')
 
+# MacBook:userscripts Ammar$ python
+# Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27)
+# [Clang 6.0 (clang-600.0.57)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import outreachyscript
+# >>> import pywikibot
+# >>> enwiki = pywikibot.Site('en', 'wikipedia')
+# >>> wikidata = enwiki.data_repository()
+# >>> outreachyscript.append_hello(wikidata, 'User:Ammarpad/Outreachy 1')
+# Page [[wikidata:User:Ammarpad/Outreachy 1]] saved
+# Page has been saved
+# 1
+# >>>
 
 # Load sandbox item. Print its name and English label
 load_wikidata_item(wikidata, 'Q4115189'):
 
+# MacBook:userscripts Ammar$ python
+# Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27)
+# [Clang 6.0 (clang-600.0.57)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import outreachyscript
+# >>> import pywikibot
+# >>> enwiki = pywikibot.Site('en', 'wikipedia')
+# >>> wikidata = enwiki.data_repository()
+# >>> outreachyscript.load_wikidata_item(wikidata, 'Q4115189')
+# The item title is: Q4115189
+# Name: Wikidata Sandbox
+# >>>
+
 # Add claim (prop: P31, value: Q5) to sandbox item (Q4115189)
 # https://www.wikidata.org/w/index.php?title=Q4115189&diff=prev&oldid=1393629723&diffmode=source
 add_claim_to_item(wikidata, 'Q4115189', 'P31', 'Q5')
+
+# MacBook:userscripts Ammar$ python
+# Python 3.8.0 (v3.8.0:fa919fdf25, Oct 14 2019, 10:23:27)
+# [Clang 6.0 (clang-600.0.57)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> import outreachyscript
+# >>> import pywikibot
+# >>> enwiki = pywikibot.Site('en', 'wikipedia')
+# >>> wikidata = enwiki.data_repository()
+# >>> outreachyscript.add_claim_to_item(wikidata, 'Q4115189', 'P31', 'Q5')
+# New claim saved!
+# >>>
